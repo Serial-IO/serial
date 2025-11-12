ff6d68fc5cc8eabfc6464120e894371ca38c12e1
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-11-12 15:41:49 +0100

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-export * as connectionProperties from './connection_properties/index.ts'
+// export { baudrates, dataBits, parities, stopBits } from './connection_properties/index.ts';
+// export type { Baudrate, DataBits, Device, Parity, StopBits } from './connection_properties/index.ts';
