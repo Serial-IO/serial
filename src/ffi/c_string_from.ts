93f6c3f7c2cf65d95fc7a972496790d8ef08012b
Mqxx <62719703+Mqxx@users.noreply.github.com>
2026-09-10 16:17:34 +0200

    feat: schema parsing

--- src/ffi/c_string_from.ts
+++
@@ -1,3 +0,0 @@
-export function cStringFrom(ptr: Deno.PointerValue): string | null {
-  return ptr === null ? null : Deno.UnsafePointerView.getCString(ptr);
-}
--- src/ffi/error_callback.ts
+++
@@ -1,4 +0,0 @@
-export type ErrorCallback = (
-  errorCode : number,
-  message : string
-) => void
